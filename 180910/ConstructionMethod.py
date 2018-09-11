8a1b7252150c4cd9b8c3422eb01c56bb53d0b94f
houhaixian <houhaixian@baidu.com>
2018-09-11 20:14:57 +0800

    learn more

--- 180910/ConstructionMethod.py
+++ 180910/ConstructionMethod.py
@@ -18,11 +18,49 @@ class FooBar:
 f = FooBar()
 # print(f.somevar)
 f = FooBar("This is a constructor argument")
-print(f.somevar)
+# print(f.somevar)
+
+
 # __del__析构方法：它在对象就要被垃圾回收之前调用。因调用的时间不可知，尽量避免使用。
 
 class A:
     def hello(self):
         print("Hello,I'm A.")
+
+
 class B(A):
     pass
+
+
+a = A()
+b = B()
+# a.hello()
+# b.hello()
+class B(A):
+    def hello(self):
+        print("Hello,I'm B.")
+b=B()
+# b.hello()
+
+class Bird:
+    def __init__(self):
+        self.hurry=True
+    def eat(self):
+        if self.hurry:
+            print('Aaaah...')
+            self.hurry=False
+        else:
+            print('No,thanks!')
+b=Bird()
+b.eat()
+b.eat()
+class SongBird(Bird):
+    def __init__(self):
+        Bird.__init__(self)
+        self.sound='Squawk!'
+    def sing(self):
+        print(self.sound)
+sb=SongBird()
+sb.sing()
+sb.eat()
+sb.eat()
---
+++ 180910/superfunction.py
@@ -0,0 +1,28 @@
+# 使用super函数调用父类构造函数
+# __metaclass__ = type
+
+class Bird:
+    def __init__(self):
+        self.hurry = True
+
+    def eat(self):
+        if self.hurry:
+            print('Aaah...')
+            self.hurry = False
+        else:
+            print('No,thanks!')
+
+
+class SongBird(Bird):
+    def __init__(self):
+        super(SongBird, self).__init__()
+        self.sound = 'Squawk!'
+
+    def sing(self):
+        print(self.sound)
+
+
+sb = SongBird()
+sb.sing()
+sb.eat()
+sb.eat()
