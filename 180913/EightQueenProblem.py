5e1eb47ce0022cc2139849fcee23d8a03aa6c614
duheng <emaildh@163.com>
2018-09-19 22:05:14 +0800

    modify

--- 180913/EightQueenProblem.py
+++ 180913/EightQueenProblem.py
@@ -9,14 +9,47 @@ def conflict(state, nextX):
     return False
 
 
-# 基本情况
-def queens(num, state):
-    if len(state) == num - 1:
-        for pos in range(num):
-            if not conflict(state, pos):
-                yield pos
+# # 基本情况
+# def queens(num, state):
+#     if len(state) == num - 1:
+#         for pos in range(num):
+#             if not conflict(state, pos):
+#                 yield pos
+#     # print(list(queens(4, (1, 3, 0))))
+#     # 需要递归的情况
+#     else:
+#         for pos in range(num):
+#             if not conflict(state, pos):
+#                 for result in queens(num, state + (pos,)):
+#                     yield (pos,) + result
+def queens(num=8, state=()):
+    for pos in range(num):
+        if not conflict(state, pos):
+            if len(state) == num - 1:
+                yield (pos,)
+            else:
+                for result in queens(num, state + (pos,)):
+                    yield (pos,) + result
 
 
-print(list(queens(4, (1, 3, 0))))
+# print(list(queens(3)))
+# print(list(queens(4)))
+# for solution in queens(8):
+#     print(solution)
 
-# 需要递归的情况
+
+# print(len(list(queens(8))))
+
+
+# 打包
+def prettyprint(solution):
+    def line(pos, length=len(solution)):
+        return '.' * (pos) + 'X' + "." * (length - pos - 1)
+
+    for pos in solution:
+        print(line(pos))
+
+
+import random
+
+prettyprint(random.choice(list(queens(8))))
