abef83b8ded61f1bf356b3f80db8be31ace8dff4
duheng <emaildh@163.com>
2018-10-09 23:36:56 +0800

    change

---
+++ 180915/random_demo.py
@@ -0,0 +1,8 @@
+# random模块包括返回随机数的函数，可用于模拟或者用于任何产生随机输出的程序。
+# random()返回0<n≤1之间的随机实数n
+# getrandbits(n)以长整型形式返回n个随机位
+# uniform(a,b)返回随机实数n，其中a≤n<b
+# randrange(start,stop,step)返回range(start,stop,step)中的随机数
+# choice（seq）从序列seq中返回随意元素
+# shuffle(seq,random)原地指定序列seq，将给定序列的元素进行随机移位，每种排序的可能性都是近似相等的。
+# sample(seq,n)从序列seq中选择n个随机且独立的元素。从给定序列中（均一地）选择给定数目的元素，同时确保元素互不相同。
--- 180915/timedemo.py
+++ 180915/timedemo.py
@@ -13,19 +13,28 @@
 import time
 
 # asctime([tuple])将时间元组转换为字符串
+# time.asctime(时间元组)，默认为当前时间
 print(time.asctime())
 # Mon Oct  8 23:34:55 2018
 
 # loaltime([secs])将秒数转换为本地时间的日期元组。如果想获得全球统一时间，则可以使用gmtime.
-print(time.localtime())
+print(time.localtime(time.time()))
 # time.struct_time(tm_year=2018, tm_mon=10, tm_mday=8, tm_hour=23, tm_min=36, tm_sec=8, tm_wday=0, tm_yday=281, tm_isdst=0)
 
 # mktime(tuple)将日期元组转换为本地时间
+# （4）时间元组 → 时间戳
+# 1539092310.1404312
+time.mktime((2018, 10, 9, 21, 38, 10, 6, 273, 0))
 
 # sleep(secs)休眠，让解释器等待给定的秒数
 
 # strptime(string,format) 将字符串解析为时间元组
+# （6）时间元组 → 可视化时间（定制）
+# time.strftime(要转换成的格式，时间元组)
+print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+# 2018-10-09 22:05:19
 
 # time()当前时间
-
+print(time.time())
+# 1539093919.751929
 # 支持日期和时间的算法：datime ;帮助开发人员对代码段的执行时间进行计时：timeit
