d8ad0c5e3cfe1cc583c6a88b26d78d60a61d875c
duheng <emaildh@163.com>
2018-10-16 18:01:46 +0800

    1016learn more code

--- 180916/re_demo.py
+++ 180916/re_demo.py
@@ -18,8 +18,21 @@ print(re.split('[,]+', some_text, maxsplit=3))
 # ['alpha', 'beta', 'gamma delta']
 pat = '[a-zA-Z]+'
 text = '"Hm...Err -- are you sure?" he said, sounding insecure.'
-print(re.findall(pat,text))
+print(re.findall(pat, text))
 # ['Hm', 'Err', 'are', 'you', 'sure', 'he', 'said', 'sounding', 'insecure']
-pat0=r'[.?\-",]+'
-print(re.findall(pat0,text))
+# 横线（-）被转义了，不会将其解释为字符范围的一部分（比如a~z）。
+pat0 = r'[.?\-",]+'
+print(re.findall(pat0, text))
 # ['"', '...', '--', '?"', ',', '.']
+
+# 函数re.sub的作用在于：使用给定的替换内容将匹配模式的子字符串（最左端并且非重叠的子字符串）替换掉。
+pat1 = '{name}'
+text1 = 'Dear {name}...'
+print(re.sub(pat1, 'Mr. Gumby', text1))
+# Dear Mr. Gumby...
+
+# re.escape可以对字符串中所有可能被解释为正则运算符的字符进行转义的应用函数。
+print(re.escape('www.python.org'))
+# www\.python\.org
+print(re.escape('But where is the ambiguity?'))
+# But\ where\ is\ the\ ambiguity\?
