df5f4edf503d654c4df3081dd714e6debfcf9fc2
duheng <emaildh@163.com>
2018-11-09 19:07:43 +0800

    change

--- 180918/thinterdemo.py
+++ 180918/thinterdemo.py
@@ -1,6 +1,5 @@
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
-from tkinter import *
 
 
 class Btn_def():
@@ -38,3 +37,25 @@ btn1 = Button(top, text='open', command=lambda: Btn.load(filename.get(), content
 btn2 = Button(top, text='save', command=lambda: Btn.save(filename.get(), contents)).pack(side=RIGHT)
 
 top.mainloop()
+
+# 知识要点：
+#
+# 一、pack()
+#
+# 参数side：指定将控件停靠在哪一条边上,LEFT、RIGHT、TOP或BOTTOM
+#
+# 参数expand：让控件随父控件（这里是窗口）一起增大，可将expand设为True
+#
+# 二、ScrolledText控件
+#
+# 编辑区域
+#
+# 三、Entry控件
+#
+# 要提取Entry控件的内容，可以使用get方法
+#
+# 四、Button控件
+#
+# 参数text：设置控件的名字
+#
+# 参数command：控件的事件处理，注意，command传递参数是使用lambda:，否则控件自动执行
