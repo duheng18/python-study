c10bff2bd2393c9e665e8697317b53e0dc6014d8
duheng <emaildh@163.com>
2019-04-15 07:51:12 +0800

    change

--- 190413/requestsDemo.py
+++ 190413/requestsDemo.py
@@ -1,5 +1,8 @@
 import requests
 
+#获得一个网页最简单的一行代码
+#通过给定get方法和url来构造一个向服务器请求资源的Request对象，这个对象是Request库内部生产的
+#返回的内容用一个变量r来表示，r是一个esponse对象，response对象包含从服务器返回的所有的相关资源
 r = requests.get("http://www.baidu.com")
 # r.status_code HTTP请求的返回状态，200表示连接成功，404表示失败
 print(r.status_code)
@@ -15,3 +18,5 @@ print(r.content)
 # print(r.headers)
 r.encoding='utf-8'
 print(r.text)
+
+#http://www.python-requests.org
