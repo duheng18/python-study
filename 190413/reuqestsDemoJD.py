5dc83ffce5688c968c9f994771e5ad3974974f94
duheng <emaildh@163.com>
2019-04-22 20:51:58 +0800

    change

---
+++ 190413/reuqestsDemoJD.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+import requests
+
+url = "https://item.jd.com/8735304.html"
+try:
+    r = requests.get(url)
+    r.raise_for_status()
+    r.encoding = r.apparent_encoding
+    print(r.text)
+except:
+    print("爬取失败")
