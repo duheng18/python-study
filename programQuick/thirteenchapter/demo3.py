47a3642939c8b5650948bed109f891b00db5de78
duheng <emaildh@163.com>
2019-11-13 22:21:03 +0800

    commit2

---
+++ programQuick/thirteenchapter/demo3.py
@@ -0,0 +1,20 @@
+import PyPDF2
+
+pdf1file = open('meetingminutes.pdf', 'rb')
+pdf2file = open('meetingminutes2.pdf', 'rb')
+pdf1Reader = PyPDF2.PdfFileReader(pdf1file)
+pdf2Reader = PyPDF2.PdfFileReader(pdf2file)
+pdfWriter = PyPDF2.PdfFileWriter()
+
+for pageNum in range(pdf1Reader.numPages):
+    pageObj = pdf1Reader.getPage(pageNum)
+    pdfWriter.addPage(pageObj)
+
+for pageNum in range(pdf2Reader.numPages):
+    pageObj = pdf2Reader.getPage(pageNum)
+    pdfWriter.addPage(pageObj)
+pdfOutputFile=open('combinedminutes.pdf','wb')
+pdfWriter.write(pdfOutputFile)
+pdfOutputFile.close()
+pdf1file.close()
+pdf2file.close()
